d85b8357a343c6dcf5761297e9e5fcd7cee003e3
Keith Serven <kserven@gmail.com>
2017-12-01 09:28:39 -0800

    Player shooting and destroy enemy

---
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float health = 150;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Projectile missle = collider.gameObject.GetComponent<Projectile>();
+        if (missle)
+        {
+            missle.Hit();
+            health -= missle.GetDamage();
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
